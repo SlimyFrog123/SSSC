b125e993b07341c9b0b3482d89ab20828f585945
DJ Cook <danieldcookjr@gmail.com>
2022-06-21 19:08:24 -0700

    Made stations refresh at an interval, (commented out) fetch data of stations running at an interval

--- sssc.js
+++ sssc.js
@@ -2,30 +2,36 @@ let stations = [
     {
         'station_name': 'A',
         'station_description': 'Station A',
-        'station_id': 1
+        'station_id': 1,
+        'running': false
     },
     {
         'station_name': 'B',
         'station_description': 'Station B',
-        'station_id': 2
+        'station_id': 2,
+        'running': false
     },
     {
         'station_name': 'C',
         'station_description': 'Station C',
-        'station_id': 3
+        'station_id': 3,
+        'running': true
     },
     {
         'station_name': 'D',
         'station_description': 'Station D',
-        'station_id': 4
+        'station_id': 4,
+        'running': false
     },
     {
         'station_name': 'E',
         'station_description': 'Station E',
-        'station_id': 5
+        'station_id': 5,
+        'running': false
     }
 ];
 
+let SSSC;
 let stationsContainer;
 
 
@@ -52,11 +58,12 @@ class SprinklerController {
     <br>
     
     <p class="station-description">${station.station_description}</p>
-    <p class="station-status">Status: <span class="status-text running">Running</span></p>
+    <p class="station-status">Status: <span class="status-text">Not Running</span></p>
 
     <div class="app-btn-container">
         <button class="app-btn min-width-100" onclick="stopStation(${station.station_id})">Stop</button>
         <select class="app-btn min-width-100" id="station-time-${station.station_id}">
+            <option value="-1">Until Stopped</option>
             <option value="1">1 Minute</option>
             <option value="5">5 Minutes</option>
             <option value="10">10 Minutes</option>
@@ -99,13 +106,42 @@ class SprinklerController {
             callback(data, status);
         });
     }
+
+    refreshStations() {
+        this.stations.forEach(station => {
+            this.getStatus(station.station_id, function(data, status) {
+                console.log(data);
+                console.log(status);
+                // this.stations[(stationID - 1)].running = data === 0 ? false : true; // If the data response === 0 (not running), set the running property to false, if it is, set it to true
+            });
+        });
+    }
+
+    refreshStationPopups() {
+        this.stations.forEach(station => {
+            this.updateStationPopup(station.station_id)
+        });
+    }
+
+    updateStationPopup(stationID) {
+        let stationPopup = $(`#station-popup-${stationID}`).find('.station-status').find('.status-text');
+        stationPopup.text(this.stations[(stationID - 1)].running ? "Running" : "Not Running");
+    }
 }
 
 $(document).ready(function() {
     stationsContainer = $('#stations-container'); // Get the stations container
     
-    let SSSC = new SprinklerController(stations, "http://127.0.0.1/sip/", "password", stationsContainer);
+    SSSC = new SprinklerController(stations, "http://127.0.0.1/sip/", "password", stationsContainer);
     SSSC.init(); // Initialize
+
+    setInterval(function() {
+        SSSC.refreshStationPopups(); // Refresh the data in the station popups
+    }, 10);
+
+    setInterval(function() {
+        // SSSC.refreshStations(); // Refresh the stations (check if they are running or not)
+    }, 500);
 });
 
 function stationClicked(stationID) {
@@ -118,4 +154,22 @@ function toggleStationPopup(stationID) {
     
     stationPopup.toggleClass('show-popup');
     stationPopupBlur.toggleClass('visible');
+}
+
+function startStation(stationID) {
+    let timeAmount = Number($(`#station-time-${stationID}`).find(':selected').val());
+    
+    if (timeAmount === -1) {
+        alert(`Station ${stationID} set to run until stopped.`);
+        SSSC.startStation(stationID);
+    } else {
+        alert(`Station ${stationID} set to run for ${timeAmount} ${timeAmount === 1 ? "minute" : "minutes"}.`);
+        let secondsAmount = (timeAmount * 60); // Get the amount of seconds to run for
+        SSSC.runStation(stationID, secondsAmount); // Run the station for the specified amount of time
+    }
+}
+
+function stopStation(stationID) {
+    alert(`Station ${stationID} stopped.`);
+    SSSC.stopStation(stationID);
 }
